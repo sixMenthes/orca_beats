29d0a7a46126eafd60692471a5c7557e2a9a62b7
sixmenthes <six.menthes@gmail.com>
2026-03-09 18:53:53 +0100

    torchcodec upgrade

--- dataloader.py
+++ dataloader.py
@@ -1,10 +1,12 @@
 import os 
 import polars as pl
-import torch
+import torchaudio
 from torch.utils.data import Dataset
 from torchaudio.transforms import Resample
-import librosa
-import soundfile as sf
+from torchcodec.decoders import AudioDecoder
+
+import warnings
+warnings.filterwarnings("ignore", message=".*has been deprecated.*")
 
 params = {
     'clipDur':3,
@@ -22,17 +24,26 @@ class LocalBirdnetDataset(Dataset):
         return self.labels.shape[0]
 
     def __getitem__(self, idx):
+        
+        audio_path = os.path.join(self.audio_dir, self.labels['pos_FilePath'][idx])
+        new_start_frame, new_end_frame = self.get_start(audio_path, idx)
+        audio_data, sample_rate = torchaudio.load(
+            audio_path,
+            frame_offset=new_start_frame,
+            num_frames=new_end_frame - new_start_frame)
+        transform = Resample(sample_rate, params['outSr'])
+        audio_data = transform(audio_data)
+        label = self.labels['Ecotype'][idx]
+        return audio_data, label
+        
+    def get_start(self, audio_path, idx):
         row = self.labels[:][idx]
-        file_path = row['FilePath']
         start_time = row['FileBeginSec']
         end_time = row['FileEndSec']
-        dep = row['Dataset']
-        provider = row['Provider']
-        speciesFolder = row['Labels']
-        soundfile = row['Soundfile']
-        CT = row['CalltypeCategory']
-        audio_path = os.path.join(self.audio_dir, self.labels['pos_FilePath'][idx])
-        file_duration = librosa.get_duration(path=audio_path)
+        decoder = AudioDecoder(audio_path)
+        metadata = decoder.metadata
+        file_duration = metadata.duration_seconds
+        sample_rate = metadata.sample_rate
         duration = end_time - start_time
         center_time = start_time + duration / 2.0
         new_start_time = max(0, center_time - params['clipDur'] / 2.0)
@@ -40,15 +51,6 @@ class LocalBirdnetDataset(Dataset):
         if new_end_time > file_duration:
             new_start_time = max(0, file_duration - params['clipDur'])
             new_end_time = file_duration
+        return (new_start_time*sample_rate, new_end_time*sample_rate)
 
-        audio_data, sample_rate = librosa.load(
-            audio_path,
-            sr = params['outSR'],
-            offset=new_start_time,
-            duration=params['clipDur'],
-            mono=False
-        )
-        #apply pre-processing to audio_path
-        label = self.labels['Ecotype'][idx]
-        
 
